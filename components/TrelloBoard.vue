6a98ec7b1dd6cf60b4d7e3d6df2519e98ce91c23
eimwe <prokopenkoserge@inbox.ru>
2023-03-19 22:24:45 +0600

    Provided board data mock and added TrelloBoard component

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import type { Column } from '~/types';
+import { nanoid } from 'nanoid';
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
+        id: nanoid(),
+        title: 'Create marketing landing page',
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: 'Develop cool new feature',
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: 'Fix page nav bug',
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: 'Selected for Dev',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'In Progress',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'QA',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Complete',
+    tasks: [],
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <div v-for="column in columns" :key="column.id">
+      <header>
+        {{ column.title }}
+      </header>
+      <p v-for="task in column.tasks" :key="task.id">
+        {{ task.title }}
+      </p>
+    </div>
+  </div>
+</template>
