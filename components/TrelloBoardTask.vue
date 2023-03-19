ca8bcd06a46bb2157351b5befc147e540e59ca7c
eimwe <prokopenkoserge@inbox.ru>
2023-03-20 00:08:16 +0600

    Added draggable styles for tasks

--- components/TrelloBoardTask.vue
+++ components/TrelloBoardTask.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <div
     :title="task.createdAt.toLocaleDateString()"
-    class="bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
+    class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
   >
     <DragHandle class="pr-2" />
     <span>
@@ -16,3 +16,22 @@ defineProps<{
     </span>
   </div>
 </template>
+
+<style>
+  .sortable-drag .task {
+    transform: rotate(5deg);
+  }
+
+  .sortable-ghost .task {
+    position: relative;
+  }
+
+  .sortable-ghost .task::after {
+    content: '';
+    /*@apply absolute top-0 right-0 bottom-0 left-0 bg-slate-300 rounded*/
+    position: absolute;
+    inset: 0;
+    border-radius: 0.25rem;
+    background-color: rgb(203 213 225);
+  }
+</style>
