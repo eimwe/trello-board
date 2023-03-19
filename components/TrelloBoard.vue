23ac1b04b76a60ecd4efc736806ca845d109ed8b
eimwe <prokopenkoserge@inbox.ru>
2023-03-19 23:26:01 +0600

    Inserted columns as slot to Vue Draggable

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Column } from '~/types';
+import draggable from 'vuedraggable';
 import { nanoid } from 'nanoid';
 const columns = ref<Column[]>([
   {
@@ -47,17 +48,24 @@ const columns = ref<Column[]>([
 </script>
 
 <template>
-  <div class="flex gap-4 overflow-x-auto items-start">
-    <div v-for="column in columns" :key="column.id"
-      class="bg-gray-200 p-5 rounded min-w-[250px]"
+  <div>
+    <draggable
+      v-model="columns"
+      group="columns"
+      item-key="id"
+      class="flex gap-4 overflow-x-auto items-start"
     >
-      <header class="font-bold mb-4">
-        {{ column.title }}
-      </header>
-      <TrelloBoardTask v-for="task in column.tasks" :task="task" :key="task.id" />
-      <footer>
-        <button class="text-gray-500">+ Add A Card</button>
-      </footer>
-    </div>
+      <template #item="{element: column}: { element: Column }">
+        <div class="bg-gray-200 p-5 rounded min-w-[250px]">
+          <header class="font-bold mb-4">
+            {{ column.title }}
+          </header>
+          <TrelloBoardTask v-for="task in column.tasks" :task="task" :key="task.id" />
+          <footer>
+            <button class="text-gray-500">+ Add A Card</button>
+          </footer>
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
