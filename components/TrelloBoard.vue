aeef56cfa283b006e294de232c36a6c4b4b93965
eimwe <prokopenkoserge@inbox.ru>
2023-03-22 15:06:27 +0600

    Added composable to save data to localStorage

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -2,7 +2,7 @@
 import type { Column, Task } from '~/types';
 import draggable from 'vuedraggable';
 import { nanoid } from 'nanoid';
-const columns = ref<Column[]>([
+const columns = useLocalStorage<Column[]>('trelloBoard', [
   {
     id: nanoid(),
     title: 'Backlog',
